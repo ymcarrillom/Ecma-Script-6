b53efafc3a89966fdb42e5cf9507114dc5497a23
yonatan <carri_2286@hotmail.com>
2025-11-01 22:55:09 -0500

    rest operatiosn and spread

--- src/es6/04.-rest-spread.js
+++ src/es6/04.-rest-spread.js
@@ -8,3 +8,20 @@ console.log(a,fruits[1])
 let user = {username : 'oscar', age: 14}
 let {username, age} = user
 console.log(username, user.age)
+
+//spread operator
+let person = { name: 'yonatan', age: 28}
+let country = 'MX'
+
+let data = {...person, country}
+console.log(data)
+
+// rest
+
+function sum (num, ...values){
+    console.log(values)
+    console.log(num + values[0])
+    return num + values[2]
+}
+
+sum(1, 1, 3)
