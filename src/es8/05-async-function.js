de0b2e6ef2bdb71aa9fc041798934a829b626eae
yonatan <carri_2286@hotmail.com>
2025-11-03 07:07:43 -0500

    async function

---
+++ src/es8/05-async-function.js
@@ -0,0 +1,18 @@
+const fnAsync = () => {
+    return new Promise ((resolve, reject) => {
+        (true)
+           ? setTimeout (() => resolve ('¡Async!'),2000 )
+           :reject(new Error ('¡Error!'));
+    });
+}
+
+const anotherFunction = async () => {
+    const something = await fnAsync();
+    console.log(something)
+    console.log('Hello')
+}
+
+console.log('before')
+anotherFunction()
+console.log('After')
+
